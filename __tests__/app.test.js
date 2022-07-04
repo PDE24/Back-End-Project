538b7fdc7994a7bae5c31e2133790bf2364d91e7
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-07-04 15:44:16 +0100

    add test for specific values

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,16 +13,16 @@ describe("GET /api/categories", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
-      .then(({ body }) => {
-        expect(body).toBeInstanceOf(Array);
+      .then(({ body: { categories } }) => {
+        expect(categories).toBeInstanceOf(Array);
       });
   });
   test("200: array contains objects with slug and decription keys", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
-      .then(({ body }) => {
-        body.forEach((category) => {
+      .then(({ body: { categories } }) => {
+        categories.forEach((category) => {
           expect(typeof category).toBe("object");
           expect(category).toHaveProperty("slug");
           expect(category).toHaveProperty("description");
@@ -40,7 +40,7 @@ describe("GET /api/categories", () => {
   });
 });
 
-describe.only("GET /api/reviews/:review_id", () => {
+describe("GET /api/reviews/:review_id", () => {
   test("200: returns an object", () => {
     return request(app)
       .get("/api/reviews/1")
@@ -49,8 +49,11 @@ describe.only("GET /api/reviews/:review_id", () => {
         expect(body).toBeInstanceOf(Object);
       });
   });
-  test("200: has correct properties", () => {
-    return request(app).get("/api/reviews/1").expect(200).then(({body}) => {
+  test("200: has correct keys on the objects", () => {
+    return request(app)
+      .get("/api/reviews/1")
+      .expect(200)
+      .then(({ body }) => {
         expect(body).toHaveProperty("review_id");
         expect(body).toHaveProperty("title");
         expect(body).toHaveProperty("review_body");
@@ -60,7 +63,25 @@ describe.only("GET /api/reviews/:review_id", () => {
         expect(body).toHaveProperty("category");
         expect(body).toHaveProperty("owner");
         expect(body).toHaveProperty("created_at");
-    })
-    
+      });
+  });
+  test("200: property values are correct", () => {
+    return request(app)
+      .get("/api/reviews/3")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual({
+          review_id: 3,
+          title: "Ultimate Werewolf",
+          designer: "Akihisa Okui",
+          owner: "bainesface",
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          review_body: "We couldn't find the werewolf!",
+          category: "social deduction",
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 5,
+        });
+      });
   });
 });
--- api/controllers/categories.controllers.js
+++ api/controllers/categories.controllers.js
@@ -3,10 +3,9 @@ const { selectAllCategories } = require("../models/categories.models");
 exports.getCategories = (req, res) => {
   selectAllCategories()
     .then((categories) => {
-      res.status(200).send(categories);
+      res.status(200).send({categories});
     })
     .catch((err) => {
       next(err);
     });
 };
-
