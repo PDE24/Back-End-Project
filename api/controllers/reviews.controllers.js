690184b72817265d49c415068ce5cd2826ef09da
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-07-04 15:03:05 +0100

    add reviews.models file with selectReviewById funtion

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -1,6 +1,7 @@
+const { selectReviewById } = require("../models/reviews.models");
 
 exports.getReviewById = (res, req) => {
-    selectReviewById().then((review) => {
-        res.status(200).send(review)
-    })
-}
+  selectReviewById().then((review) => {
+    res.status(200).send(review);
+  });
+};
