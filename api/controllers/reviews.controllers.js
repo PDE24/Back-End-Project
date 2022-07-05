4805eef29c7927b122298f87ed60c192b50d03ba
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-07-05 14:45:04 +0100

    add 400 error handling Must provide a number

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -14,3 +14,15 @@ exports.getReviewById = (req, res, next) => {
     });
 };
 
+exports.patchReviewVotes = (req, res, next) => {
+  const { review_id } = req.params;
+  const updateBy = req.body.inc_votes;
+
+  updateReviewVotes(review_id, updateBy)
+    .then((review) => {
+      res.status(202).send({ review });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
--- api/models/reviews.models.js
+++ api/models/reviews.models.js
@@ -1,5 +1,6 @@
 const connection = require("../../db/connection");
 
+
 exports.selectReviewById = (reviewId) => {
   return connection
     .query(
@@ -20,3 +21,31 @@ exports.selectReviewById = (reviewId) => {
     });
 };
 
+exports.updateReviewVotes = (review_id, updateBy) => {
+  if (typeof updateBy !== "number") {
+    return Promise.reject({
+      status: 400,
+      msg: "Must provide a number to update votes",
+    });
+  }
+
+  return connection
+    .query(
+      `
+  UPDATE reviews
+  SET votes = votes + $2
+  WHERE review_id = $1
+  RETURNING *
+  `,
+      [review_id, updateBy]
+    )
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `Review ${review_id} does not exist`,
+        });
+      }
+      return result.rows[0];
+    });
+};
