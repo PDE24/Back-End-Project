69454b61531cd1fef983e4df19582d6467c61e53
PDE24 <95310165+PDE24@users.noreply.github.com>
2022-07-07 14:22:49 +0100

    add files

--- api/controllers/error_handling.controller.js
+++ api/controllers/error_handling.controller.js
@@ -21,5 +21,6 @@ exports.handlePSQLErrors = (err, req, res, next) => {
 };
 
 exports.handle500Errors = (err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ msg: "Server Error" });
 };
